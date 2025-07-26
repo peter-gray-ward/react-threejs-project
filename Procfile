92679d923da640236580a0ac53585249f7ae36be
peter-gray-ward <130243356+peter-gray-ward@users.noreply.github.com>
2025-07-26 00:52:58 -0400

    ...

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar target/reactthreeproject-0.0.1-SNAPSHOT.jar
--- azure-publish.sh
+++ azure-publish.sh
@@ -1,1 +1,3 @@
 scp -i ./finance-calendar_key.pem -r ./ azureuser@172.171.243.235:~/game
+scp -i ../finance-calendar_key.pem -r ./src/components azureuser@172.171.243.235:~/game/components
+ssh ../finance-calendar_key.pem azureuser@172.171.243.235
